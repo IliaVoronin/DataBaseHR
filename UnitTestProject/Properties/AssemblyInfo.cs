00ca621cc4a2d0e7aa7842591d37772b7c52dab2
DESKTOP-OK3BIT4\admin <iliavoronin9898@gmail.com>
2023-04-18 21:54:47 +0300

    more unit tests

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d1c2d73b-69f8-4cc1-ba79-fc4f85054576")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataBaseHR.Tests
+{
+    [TestClass]
+    public class EncryptStringTests
+    {
+        [TestMethod]
+        public void encrypt_same_key_test()
+        {
+            //arrange
+            string passwordTest = "qwerty1234";
+
+            //act
+            string encryptedPassword1 = EncryptionDecryptionUsingSymmetricKey.AesOperation.EncryptString("b14ca5898a4e4133bbce2ea2315a1916", passwordTest);
+            string encryptedPassword2 = EncryptionDecryptionUsingSymmetricKey.AesOperation.EncryptString("b14ca5898a4e4133bbce2ea2315a1916", passwordTest);
+
+            //assert
+            Assert.AreEqual(encryptedPassword1, encryptedPassword2);
+        }
+
+        [TestMethod]
+        public void encrypt_different_key_test()
+        {
+            //arrange
+            string passwordTest = "qwerty1234";
+
+            //act
+            string encryptedPassword1 = EncryptionDecryptionUsingSymmetricKey.AesOperation.EncryptString("b14ca5898a4e4133bbce2ea2315a1916", passwordTest);
+            string encryptedPassword2 = EncryptionDecryptionUsingSymmetricKey.AesOperation.EncryptString("vbhf234jfbdn2343234jkfnjfkfm2787", passwordTest);
+
+            //assert
+            Assert.AreNotEqual(encryptedPassword1, encryptedPassword2);
+        }
+
+        [TestMethod]
+        public void decrypt_same_key_test()
+        {
+            //arrange 
+            string passwordTest = "qwerty1234";
+            string encryptedPassword = EncryptionDecryptionUsingSymmetricKey.AesOperation.EncryptString("b14ca5898a4e4133bbce2ea2315a1916", passwordTest);
+
+            //act
+            string decryptedPassword = EncryptionDecryptionUsingSymmetricKey.AesOperation.DecryptString("b14ca5898a4e4133bbce2ea2315a1916", encryptedPassword);
+
+            //assert
+            Assert.AreEqual(passwordTest, decryptedPassword);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.Security.Cryptography.CryptographicException))]
+        public void decrypt_different_key_test()
+        {
+            //arrange 
+            string passwordTest = "qwerty1234";
+            string encryptedPassword = EncryptionDecryptionUsingSymmetricKey.AesOperation.EncryptString("b14ca5898a4e4133bbce2ea2315a1916", passwordTest);
+
+            //act
+            string decryptedPassword = EncryptionDecryptionUsingSymmetricKey.AesOperation.DecryptString("vbhf234jfbdn2343234jkfnjfkfm2787", encryptedPassword);
+
+            //assert - Expects exception
+        }
+    }
+}
