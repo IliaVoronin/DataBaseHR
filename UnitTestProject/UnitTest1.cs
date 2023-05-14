d38802a40eb862d779e1913f1d9b86d3b151d383
Ilia Voronin <iliavoronin9898@gmail.com>
2023-05-14 10:46:52 +0300

    Update UnitTest1.cs

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -3,63 +3,20 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DataBaseHR.Tests
 {
-    [TestClass]
-    public class EncryptStringTests
+[TestClass]
+    public class DatabaseTests
     {
-        [TestMethod]
-        public void encrypt_same_key_test()
-        {
-            //arrange
-            string passwordTest = "qwerty1234";
-
-            //act
-            string encryptedPassword1 = EncryptionDecryptionUsingSymmetricKey.AesOperation.EncryptString("b14ca5898a4e4133bbce2ea2315a1916", passwordTest);
-            string encryptedPassword2 = EncryptionDecryptionUsingSymmetricKey.AesOperation.EncryptString("b14ca5898a4e4133bbce2ea2315a1916", passwordTest);
-
-            //assert
-            Assert.AreEqual(encryptedPassword1, encryptedPassword2);
-        }
+        private const string ConnectionString = "Data Source=(localdb)\\MyLocalDB;Initial Catalog=TestDB;Integrated Security=True";
 
         [TestMethod]
-        public void encrypt_different_key_test()
+        public void TestDatabaseConnection()
         {
-            //arrange
-            string passwordTest = "qwerty1234";
-
-            //act
-            string encryptedPassword1 = EncryptionDecryptionUsingSymmetricKey.AesOperation.EncryptString("b14ca5898a4e4133bbce2ea2315a1916", passwordTest);
-            string encryptedPassword2 = EncryptionDecryptionUsingSymmetricKey.AesOperation.EncryptString("vbhf234jfbdn2343234jkfnjfkfm2787", passwordTest);
-
-            //assert
-            Assert.AreNotEqual(encryptedPassword1, encryptedPassword2);
-        }
-
-        [TestMethod]
-        public void decrypt_same_key_test()
-        {
-            //arrange 
-            string passwordTest = "qwerty1234";
-            string encryptedPassword = EncryptionDecryptionUsingSymmetricKey.AesOperation.EncryptString("b14ca5898a4e4133bbce2ea2315a1916", passwordTest);
-
-            //act
-            string decryptedPassword = EncryptionDecryptionUsingSymmetricKey.AesOperation.DecryptString("b14ca5898a4e4133bbce2ea2315a1916", encryptedPassword);
-
-            //assert
-            Assert.AreEqual(passwordTest, decryptedPassword);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(System.Security.Cryptography.CryptographicException))]
-        public void decrypt_different_key_test()
-        {
-            //arrange 
-            string passwordTest = "qwerty1234";
-            string encryptedPassword = EncryptionDecryptionUsingSymmetricKey.AesOperation.EncryptString("b14ca5898a4e4133bbce2ea2315a1916", passwordTest);
-
-            //act
-            string decryptedPassword = EncryptionDecryptionUsingSymmetricKey.AesOperation.DecryptString("vbhf234jfbdn2343234jkfnjfkfm2787", encryptedPassword);
-
-            //assert - Expects exception
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                Assert.AreEqual(ConnectionState.Open, connection.State);
+            }
         }
     }
+
 }
